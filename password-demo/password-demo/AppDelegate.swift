d3fb800725fa48181c9d7396d53b83ca6e416f86
joelzarco <joelzarco@me.com>
2022-06-30 19:11:21 -0500

    Clean teamplate for new project

---
+++ password-demo/password-demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  password-demo
+//
+//  Created by Johel Zarco on 30/06/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application : UIApplication, didFinishLaunchingWithOptions launchOptions : [UIApplication.LaunchOptionsKey : Any]?) -> Bool{
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemPink
+        window?.rootViewController = ViewController()
+        return true
+    }
+
+
+}
+
